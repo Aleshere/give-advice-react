b835161f70eed31dae345e8a2b93d351bd315723
Alessio Burgio <alessioburgio85@gmail.com>
2020-07-28 15:22:06 +0200

    Updates

---
+++ precache-manifest.5cd15ad507be7856dac581221267593a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c3a7309d11ffea48cd87214eaf206b6",
+    "url": "/Aleshere/give-advice-react/index.html"
+  },
+  {
+    "revision": "368bdf5fbca5491e993a",
+    "url": "/Aleshere/give-advice-react/static/css/main.a59745dc.chunk.css"
+  },
+  {
+    "revision": "d621d8089ecfb9eaea74",
+    "url": "/Aleshere/give-advice-react/static/js/2.d0967d56.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Aleshere/give-advice-react/static/js/2.d0967d56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "368bdf5fbca5491e993a",
+    "url": "/Aleshere/give-advice-react/static/js/main.1b8bbcd1.chunk.js"
+  },
+  {
+    "revision": "63f0209e521045570172",
+    "url": "/Aleshere/give-advice-react/static/js/runtime-main.0895d424.js"
+  },
+  {
+    "revision": "91c109fc2e2a14e1e0a445d4314f9334",
+    "url": "/Aleshere/give-advice-react/static/media/city.91c109fc.jpg"
+  }
+]);
